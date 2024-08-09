dff28953db02644593c0d954f8fcbe79af7e8687
Ankur1670 <16sainiankur@gmail.com>
2024-08-09 18:36:52 +0530

    newcommit

--- frontend/src/components/MainSection/MainSection.jsx
+++ frontend/src/components/MainSection/MainSection.jsx
@@ -8,6 +8,11 @@ import slider2 from '../../assets/slider-2.jpg'
 import slider3 from '../../assets/slider-3.jpg'
 import slider4 from '../../assets/slider-4.jpg'
 import slider5 from '../../assets/slider-5.jpg'
+import yogaImg1 from '../../assets/yogaImg-1.jpg'
+import yogaImg2 from '../../assets/yogaImg-2.jpg'
+import yogaImg3 from '../../assets/yogaImg-3.jpg'
+
+
 
 
 const slides = [
@@ -22,6 +27,8 @@ const slides = [
 
 const MainSection = () => {
   return (
+
+  <>
     <section
       className="relative bg-cover bg-no-repeat bg-center h-[550px] w-full"
       style={{ backgroundImage: `url(${mainImage})` }} // Adjust path if necessary
@@ -99,6 +106,59 @@ const MainSection = () => {
         </div>
       </div>
     </section>
+
+    <section className="mt-[250px] text-center container">
+  <div className="container mx-auto">
+    <div className="row">
+      <div className="col">
+        <h1 className="text-[35px] font-bold mb-12 heading">WHAT YOU CAN EXPECT FROM SERENESOUL YOGA COURSES</h1>
+      </div>
+    </div>
+
+    <div className="grid md:grid-cols-3 gap-8 items-center mt-12">
+      <div className="content">
+        <img src={yogaImg1} alt="Authenticity" className="lg:h-[400px] sm:h-[300px] rounded-3xl mx-auto  " />
+      </div>
+      <div className="content mt-4 md:mt-0">
+        <h2 className="font-semibold mb-4 sm:text-[20px]">AUTHENTICITY</h2>
+        <p className='text__para'>Experience the essence of true yoga with our SereneSoul Yoga Courses. We prioritize authenticity, drawing from ancient scriptures like the Bhagavad Gita and Patanjali Yoga Sutras. Our programs offer genuine teachings, ensuring a pure and transformative journey that aligns with traditional yoga practices. Perfect for dedicated yoga seekers.</p>
+      </div>
+      <div className="content mt-4 md:mt-0">
+        <img src={yogaImg2} alt="Safety and Integrity" className="lg:h-[400px] sm:h-[300px] rounded-3xl mx-auto" />
+      </div>
+    </div>
+
+    <div className="grid md:grid-cols-3 gap-8 items-center mt-12">
+      <div className="content">
+        <h2 className="font-semibold mb-4 sm:text-[20px]">SAFETY AND INTEGRITY</h2>
+        <p className='text__para '>At SereneSoul, our commitment to safety and integrity is paramount. We prioritize your well-being, offering carefully designed yoga practices that ensure both physical and emotional safety. With expert guidance, each session is crafted to prevent injury and maintain the highest standards of practice, promoting trust and holistic growth.</p>
+      </div>
+      <div className="content mt-4 md:mt-0">
+        <img src={yogaImg3} alt="Cultural Immersions" className="lg:h-[400px] sm:h-[300px] rounded-3xl mx-auto" />
+      </div>
+      <div className="content mt-4 md:mt-0">
+        <h2 className="font-semibold mb-4">CULTURAL IMMERSIONS</h2>
+        <p className='text__para'>SereneSoul invites you to embark on a transformative journey through India's rich cultural heritage. Experience authentic yoga practices, visit sacred sites, and immerse yourself in the spiritual traditions that have shaped yoga. Our guided tours offer deep insights into ancient rituals and customs, connecting you with the true essence of yoga.</p>
+      </div>
+    </div>
+
+    <div className="grid md:grid-cols-3 gap-8 items-center mt-12">
+      <div className="content">
+        <img src={yogaImg2} alt="Safety and Integrity" className="lg:h-[400px] sm:h-[300px] rounded-3xl mx-auto" />
+      </div>
+      <div className="content mt-4 md:mt-0">
+        <h2 className="font-semibold mb-4">VALUE</h2>
+        <p className='text__para'>SereneSoul provides exceptional value through our yoga retreats and teacher training programs. Investing in our courses means investing in profound personal growth and healing. Our programs deliver transformative experiences that elevate your physical, mental, and emotional well-being, offering lifelong benefits and inspiring changes in every aspect of your life.</p>
+      </div>
+      <div className="content mt-4 md:mt-0">
+        <img src={yogaImg1} alt="Authenticity" className="lg:h-[400px] sm:h-[300px] rounded-3xl mx-auto" />
+      </div>
+    </div>
+  </div>
+</section>
+
+    </>
+    
   );
 };
 
